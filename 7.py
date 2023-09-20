b92d036b1e0fffb6604f481c9376334116eae72c
chenshangxian <143639227+chenshangxian@users.noreply.github.com>
2023-09-19 18:01:32 -0700

    chapter4

---
+++ 7.py
@@ -0,0 +1,11 @@
+salary = 0
+days = int(input('Pleass enter the number of days:'))
+x = 1
+
+for a in range(1,days + 1):
+    salary += x
+    print('Salary:' , x/100, 'in day', a)
+    x *= 2
+   
+
+print('The total salary is:', salary/100)
